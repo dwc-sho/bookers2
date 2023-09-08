98498b93344b66b14a6ea6ee02afc5fb54bdca7f
EC2 Default User <ec2-user@ip-172-31-5-153.ap-northeast-1.compute.internal>
2023-09-08 08:16:13 +0000

    バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :name, length: { in: 2..20 }
+  
+  validates :introduction, length: { maximum: 50 }
 end
