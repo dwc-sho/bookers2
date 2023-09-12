95f105f9123b3a168544a25765381fde60c35dff
EC2 Default User <ec2-user@ip-172-31-5-153.ap-northeast-1.compute.internal>
2023-09-12 07:49:59 +0000

    ユーザーごとの投稿を閲覧できるように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @book = Book.new
+    @books = Book.where(user_id: params[:id])
   end
 
   def edit
