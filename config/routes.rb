413b5e2ab5232bb54a604a0ec5de05afb7ea126f
EC2 Default User <ec2-user@ip-172-31-5-153.ap-northeast-1.compute.internal>
2023-09-12 06:31:03 +0000

    Userの一覧を作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get "home/about" => "homes#about" , as: :about
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
 end
