ec16f2fe801ed8ce85210d8054e2eb10c7780407
EC2 Default User <ec2-user@ip-172-31-5-153.ap-northeast-1.compute.internal>
2023-09-12 10:48:25 +0000

    アクセス制御を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @users = User.all
     @books = Book.all
@@ -14,6 +16,9 @@ class BooksController < ApplicationController
   
   def edit
     @book = Book.find(params[:id])
+    unless @book.user_id == current_user.id
+      redirect_to books_path
+    end
   end
   
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @users = User.all
     @user = User.find(current_user.id)
     @book = Book.new
   end
-  
+
   def show
     @user = User.find(params[:id])
     @book = Book.new
@@ -13,11 +15,14 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    unless @user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
-  
+
   def update
     @user = User.find(params[:id])
-    
+
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
       redirect_to user_path
@@ -25,7 +30,7 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:name, :introduction)
