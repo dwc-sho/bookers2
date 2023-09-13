806356dc2407f13016cad64bfb2cd452ad6f696a
EC2 Default User <ec2-user@ip-172-31-5-153.ap-northeast-1.compute.internal>
2023-09-13 11:44:06 +0000

    不必要なカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_13_105031) do
+ActiveRecord::Schema.define(version: 2023_09_13_114045) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 2023_09_13_105031) do
   end
 
   create_table "favorites", force: :cascade do |t|
-    t.boolean "good", default: false
     t.integer "user_id", null: false
     t.integer "book_id", null: false
     t.datetime "created_at", precision: 6, null: false
